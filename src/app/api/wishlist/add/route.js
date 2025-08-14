e8c8ba8139f9bfdf558f68076f5c1400007b9d04
M-TALHA  BABAR <babarmtalha91@gmail.com>
2025-08-14 21:49:53 +0500

    abcd

--- src/app/api/wishlist/add/route.js
+++ src/app/api/wishlist/add/route.js
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import { connectToDB } from "@/lib/db";
+import { connectDB } from "@/lib/db";
 
 import Wishlist from "@/models/Wishlist";
 
